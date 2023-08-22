9fb82be3eb0678917ce5985d80bdf6e1678913e9
facumoyano99 <facundomoyano1999@gmail.com>
2023-08-22 19:56:47 -0300

    ValidateFile

---
+++ PeliculasAPI/Controllers/ActoresController.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using PeliculasAPI.Servicios;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/actores")]
+    public class ActoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly string contenedor = "actores";
+
+        public ActoresController(ApplicationDbContext context, IMapper mapper, IAlmacenadorArchivos almacenadorArchivos)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.almacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActorDTO>>> Get()
+        {
+            var entidades = await context.Actores.ToListAsync();
+            return mapper.Map<List<ActorDTO>>(entidades);
+        }
+
+        [HttpGet("{id:int}", Name = "obtenerActor")]
+        public async Task<ActionResult<ActorDTO>> Get(int id)
+        {
+            var entidad = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+            if (entidad == null)
+            {
+                NotFound();
+            }
+            return mapper.Map<ActorDTO>(entidad);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] ActorCreacionDTO actorCreacionDTO)
+        {
+            var entidad = mapper.Map<Actor>(actorCreacionDTO);
+
+            if (actorCreacionDTO.Foto != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await actorCreacionDTO.Foto.CopyToAsync(memoryStream);
+                    var contenido = memoryStream.ToArray();
+                    var extension = Path.GetExtension(actorCreacionDTO.Foto.FileName);
+                    entidad.Foto = await almacenadorArchivos.GuardarArchivo(contenido, extension, contenedor, actorCreacionDTO.Foto.ContentType);
+                }
+            }
+
+
+            context.Add(entidad);
+            await context.SaveChangesAsync();
+            var actorDTO = mapper.Map<ActorDTO>(entidad);
+
+            return new CreatedAtRouteResult("obtenerActor", new { id = actorDTO.Id }, actorDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromForm] ActorCreacionDTO actorCreacionDTO)
+        {
+            var actorDB = await context.Actores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (actorDB == null) { return NotFound(); }
+
+            actorDB = mapper.Map(actorCreacionDTO, actorDB);//Mappea solo los datos que se quiere editar
+
+            if (actorCreacionDTO.Foto != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await actorCreacionDTO.Foto.CopyToAsync(memoryStream);
+                    var contenido = memoryStream.ToArray();
+                    var extension = Path.GetExtension(actorCreacionDTO.Foto.FileName);
+                    actorDB.Foto = await almacenadorArchivos.EditarArchivo(contenido, extension, contenedor, actorDB.Foto ,actorCreacionDTO.Foto.ContentType);
+                }
+            }
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Actores.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Actor() { Id = id });
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ PeliculasAPI/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PeliculasAPI.Servicios;
+using System.Text.Json.Serialization;
+
+namespace PeliculasAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureService(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddTransient<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
+            services.AddHttpContextAccessor();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
