03dcc887bfd9222b765e9a6b0ffb83babac5b6b6
facumoyano99 <facundomoyano1999@gmail.com>
2023-08-29 20:55:32 -0300

    Peliculas

---
+++ PeliculasAPI/Controllers/PeliculasController.cs
@@ -0,0 +1,129 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+using PeliculasAPI.Migrations;
+using PeliculasAPI.Servicios;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/peliculas")]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+        private readonly IAlmacenadorArchivos almacenadorArchivos;
+        private readonly string contenedor = "peliculas";
+
+        public PeliculasController(ApplicationDbContext context, IMapper mapper, IAlmacenadorArchivos almacenadorArchivos)
+        {
+            this.context = context;
+            this.mapper = mapper;
+            this.almacenadorArchivos = almacenadorArchivos;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PeliculaDTO>>> Get()
+        {
+            List<Pelicula> peliculas = await context.Peliculas.ToListAsync();
+            return mapper.Map<List<PeliculaDTO>>(peliculas);
+        }
+
+        [HttpGet("{id}", Name = "obtenerPelicula")]
+        public async Task<ActionResult<PeliculaDTO>> Get(int id)
+        {
+            Pelicula pelicula = await context.Peliculas.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (pelicula != null) { return NotFound(); }
+
+            return mapper.Map<PeliculaDTO>(pelicula);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromForm] PeliculaCreacionDTO peliculaCreacionDTO)
+        {
+            Pelicula pelicula = mapper.Map<Pelicula>(peliculaCreacionDTO);
+
+            if (peliculaCreacionDTO.Poster != null)
+            {
+                using MemoryStream memoryStream = new();
+                await peliculaCreacionDTO.Poster.CopyToAsync(memoryStream);
+                byte[] contenido = memoryStream.ToArray();
+                string extension = Path.GetExtension(peliculaCreacionDTO.Poster.FileName);
+                pelicula.Poster = await almacenadorArchivos.GuardarArchivo(contenido, extension, contenedor, peliculaCreacionDTO.Poster.ContentType);
+            }
+
+            context.Add(pelicula);
+
+            PeliculaDTO peliculaDTO = mapper.Map<PeliculaDTO>(pelicula);
+
+            return new CreatedAtRouteResult("obtenerPelicula", new { id = pelicula.Id }, peliculaDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromForm] PeliculaCreacionDTO peliculaCreacionDTO)
+        {
+            Pelicula peliculaDB = await context.Peliculas.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (peliculaDB == null) { return NotFound(); }
+
+            peliculaDB = mapper.Map(peliculaCreacionDTO, peliculaDB);//Mappea solo los datos que se quiere editar
+
+            if (peliculaCreacionDTO.Poster != null)
+            {
+                using MemoryStream memoryStream = new();
+                await peliculaCreacionDTO.Poster.CopyToAsync(memoryStream);
+                byte[] contenido = memoryStream.ToArray();
+                string extension = Path.GetExtension(peliculaCreacionDTO.Poster.FileName);
+                peliculaDB.Poster = await almacenadorArchivos.EditarArchivo(contenido, extension, contenedor, peliculaDB.Poster, peliculaCreacionDTO.Poster.ContentType);
+            }
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<ActionResult> Patch(int id, [FromBody] JsonPatchDocument<PeliculaPatchDTO> patchDocument)
+        {
+            if (patchDocument == null) { return BadRequest(); }
+
+            Pelicula entidadBD = await context.Peliculas.FirstOrDefaultAsync(x => x.Id == id);
+            if (entidadBD == null) { return NotFound(); }
+
+            PeliculaPatchDTO entidadDTO = mapper.Map<PeliculaPatchDTO>(entidadBD);
+
+            patchDocument.ApplyTo(entidadDTO, ModelState);
+
+            bool esValido = TryValidateModel(entidadDTO);
+
+            if (!esValido) { return BadRequest(ModelState); }
+
+            mapper.Map(entidadDTO, entidadBD);
+
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            bool existe = await context.Peliculas.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Pelicula() { Id = id });
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
